579b645f1a2f247f7351c983b7f179c9985f34ab
Harshaan <11arshaan@gmail.com>
2023-05-11 07:42:22 -0400

    init react flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
-import { ReactFlowProvider } from 'reactflow';
+import ReactFlow, { Background } from "reactflow";
+import { shallow } from "zustand/shallow";
 
+import { useStore } from "./store/store.js";
+
+const selector = (store) => ({
+  nodes: store.nodes,
+  edges: store.edges,
+  onNodesChange: store.onNodesChange,
+  onEdgesChange: store.onEdgesChange,
+  addEdge: store.addEdge,
+});
 
 function App() {
- 
+  const store = useStore(selector, shallow);
+
   return (
     <div className="app">
-      <ReactFlowProvider>
-        <h1>React Flow</h1>
-      </ReactFlowProvider>
+      <ReactFlow
+        nodes={store.nodes}
+        edges={store.edges}
+        onNodesChange={store.onNodesChange}
+        onEdgesChange={store.onEdgesChange}
+        onConnect={store.addEdge}
+      >
+        <Background />
+      </ReactFlow>
     </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ReactFlowProvider } from "reactflow";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "reactflow/dist/style.css";
+import "./index.css";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
+  <ReactFlowProvider>
     <App />
+  </ReactFlowProvider>
   // </React.StrictMode>,
-)
+);
